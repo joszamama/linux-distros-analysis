488a7fe3e8d6d2187bbe68fbf7c73c5d8a464c0a
joszamama <joszamama@gmail.com>
2023-02-24 18:45:30 +0100

    refactor: repo lint

--- operators/count.py
+++ operators/count.py
@@ -8,9 +8,11 @@ def count_packages_apt(file):
         standard = data.count("Priority: standard")
         optional = data.count("Priority: optional")
         extra = data.count("Priority: extra")
-        undefined = packages - (required + important + standard + optional + extra)
+        undefined = packages - (required + important +
+                                standard + optional + extra)
         f.close()
         print(f"{file} has {packages} packages, {required} required, where {essential} are essential. {important} important, {standard} standard, {optional} optional, {extra} extra and {undefined} undefined.")
+
 
 def count_packages_dnf(file):
     with open(file, "r", encoding="utf-8") as f:
@@ -25,7 +27,8 @@ print("Ubuntu Packages")
 count_packages_apt("distros/ubuntu/18.04 - bionic/ubuntu-bionic-packages.txt")
 count_packages_apt("distros/ubuntu/20.04 - focal/ubuntu-focal-packages.txt")
 count_packages_apt("distros/ubuntu/22.04 - jammy/ubuntu-jammy-packages.txt")
-count_packages_apt("distros/ubuntu/22.10 - kinetic/ubuntu-kinetic-packages.txt")
+count_packages_apt(
+    "distros/ubuntu/22.10 - kinetic/ubuntu-kinetic-packages.txt")
 
 print("\n------------------")
 print("Fedora Packages")
@@ -38,6 +41,7 @@ print("Debian Packages")
 count_packages_apt("distros/debian/v8 - jessie/debian-jessie-packages.txt")
 count_packages_apt("distros/debian/v9 - stretch/debian-stretch-packages.txt")
 count_packages_apt("distros/debian/v10 - buster/debian-buster-packages.txt")
-count_packages_apt("distros/debian/v11 - bullseye/debian-bullseye-packages.txt")
+count_packages_apt(
+    "distros/debian/v11 - bullseye/debian-bullseye-packages.txt")
 
 print("\n------------------")
